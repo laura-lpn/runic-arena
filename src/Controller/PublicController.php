d9b300172636a652c9bd912037cd6b48de3e6778
laura-lpn <74555001+laura-lpn@users.noreply.github.com>
2024-11-19 07:56:20 +0100

    add style

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CardRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class PublicController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(CardRepository $cardRepository): Response
     {
-        return $this->render('public/index.html.twig');
+        $lastCards = $cardRepository->findBy([], ['id' => 'DESC'], 5);
+
+        return $this->render('public/index.html.twig', [
+            'lastCards' => $lastCards
+        ]);
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
 const defaultTheme = require('tailwindcss/defaultTheme')
-const forms = require('@tailwindcss/forms')
-
 
 module.exports = {
   content: [
@@ -10,41 +8,26 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    colors: {
-      'black': '#121212',
-      'white': '#ffffff',
-      'orange': '#edae64',
-      'orange-light': '#ffe9cf',
-      DEFAULT: '#121212'
-    },
-    fontFamily: {
-      'main': ['Montserrat', 'sans-serif'],
-      'second': ['Kodchasan', 'sans-serif'],
-      'sans': ['Montserrat', 'sans-serif',
-      defaultTheme.fontFamily.sans]
-    },
-    borderRadius: {
-      'main': '16px',
-      'input': '8px',
-    },
-    boxShadow: {
-      'main': '0px 0px 15px #00000029',
-    },
-    fontSize: {
-      xxs: '.5rem',
-      xs: '.75rem',
-      sm: '1rem',
-      base: '1.2rem',
-      lg: '1.3rem',
-      xl: '1.5rem',
-      '2xl': '1.8rem',
-      '3xl': '2rem',
-      '4xl': '3rem',
-      '5xl': '4rem',
-      '6xl': '4.5rem',
+    extend: {
+      colors: {
+        'black': '#121212',
+        'white': '#d9e6eb',
+        'green': '#34d399',
+        'dark-blue': '#111827',
+      },
+      fontFamily: {
+        'main': ['Montserrat', 'sans-serif'],
+        'second': ['Faculty Glyphic', 'sans-serif'],
+        'sans': ['Montserrat', 'sans-serif',
+        defaultTheme.fontFamily.sans]
+      },
+      boxShadow: {
+        'green': '0px 0px 15px #34d399',
+        'green-2xl': '0px 0px 30px #34d399',
+      }
     }
   },
   plugins: [
-    forms
+    require('@tailwindcss/forms')
   ],
 }
